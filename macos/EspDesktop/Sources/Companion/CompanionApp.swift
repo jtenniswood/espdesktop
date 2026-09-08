d59a6f4ac864fbae4e7c70d9c7f80eac90ac311f
jtenniswood <james@tenniswood.co.uk>
2026-09-08 17:59:46 +0100

    Add update icon to Companion menu

--- macos/EspDesktop/Sources/Companion/CompanionApp.swift
+++ macos/EspDesktop/Sources/Companion/CompanionApp.swift
@@ -113,7 +113,8 @@ final class CompanionApplicationDelegate: NSObject, NSApplicationDelegate, NSMen
 
         addMenuItem("EspDesktop Settings", action: #selector(openSettings), key: ",", to: menu)
         addMenuItem(store.updater.isChecking ? "Checking for Updates…" : "Check for Updates",
-                    action: #selector(checkForUpdates), to: menu)
+                    action: #selector(checkForUpdates),
+                    image: NSImage(systemSymbolName: "arrow.triangle.2.circlepath", accessibilityDescription: "Check for Updates"), to: menu)
         menu.items.last?.isEnabled = !store.updater.isChecking
         addMenuItem(
             "Quit App", action: #selector(quit), key: "q",
